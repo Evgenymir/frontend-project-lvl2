df2614394f1df82af5698935835291d89a532ffc
Evgeny <bespalove.mir@gmail.com>
2020-05-01 13:39:13 +0300

    Переносит тесты

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import makeDifference from '../index';
+import makeDifference from '../src/index';
 
 const dataOne = path.resolve(__dirname, '__fixtures__/before.json');
 const dataTwo = path.resolve(__dirname, '__fixtures__/after.json');
